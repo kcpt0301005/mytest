e6bffca227413365d2e92a87055218a26825adf0
lw <luowen@gmail.com>
2018-08-27 19:35:24 +0800

    第三次提交

--- demo-test/src/main/java/cn/itcast/Demo1.java
+++ demo-test/src/main/java/cn/itcast/Demo1.java
@@ -3,6 +3,6 @@ package cn.itcast;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-		System.out.println("233333");
+		System.out.println("233");
     }
 }
